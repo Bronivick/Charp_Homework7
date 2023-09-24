8893660a2d00bd0c98ad71df819aad576d685635
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-24 03:51:19 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("первая размерность массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("вторая размерность массива");
+int colomns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, colomns];
+
+int sum = 0;
+double mid = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int m = 0; m < matrix.GetLength(1); m++)
+    {
+        matrix[i,m] = new Random().Next(0,10);
+        Console.Write(matrix[i,m]+ " ");
+    }
+    Console.WriteLine();
+}
+
+
+for (int m = 0; m < matrix.GetLength(1); m++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i,m];
+        
+    }
+    mid = (double)sum/rows;
+    Console.WriteLine();
+    Console.WriteLine($"Среднее арифмическое столбца {m + 1} равно {mid}");
+    sum = 0;
+}
+
